cb8e978932d04d4c5388e17b568e8439fc070564
Abhishek Ranjan <abhishekrjnn@gmail.com>
2023-02-13 17:33:37 +0530

    app js initialisation

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 //jshint esversion:6
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+
+app.listen(3000, function(req, res){
+    console.log("App listening to port 3000");
+})
